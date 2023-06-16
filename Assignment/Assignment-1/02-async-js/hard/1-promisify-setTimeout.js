a34f33652fbfaf9bdaf79354fb9b1d386c6b143b
Harshit Choudhary <harshitchoudhary18.hc@gmail.com>
2023-06-15 21:01:33 -0500

    Assignment-1 Part-2

--- Assignment/Assignment-1/02-async-js/hard/1-promisify-setTimeout.js
+++ Assignment/Assignment-1/02-async-js/hard/1-promisify-setTimeout.js
@@ -1,13 +1,11 @@
 /*
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
-function returnPromise(){
-    return new Promise((resolve, reject)=>{
-        resolve(console.log('Promise is resolved'));
-    })
-}
+
 function wait(n) {
-    setTimeout(returnPromise, n)
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{resolve(console.log('Promise is resolved'))},n);
+    })
 }
 
 wait(5000)
